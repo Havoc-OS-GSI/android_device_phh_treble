bcee6d425b3c42a8464a1f4f2b3224c771d7fd75
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2021-11-18 02:19:52 +0000

    treble: Proper target names
    Change-Id: Ib8c12e98d4e875733f0df7874b3a14370f7166e3
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

--- generate.sh
+++ generate.sh
@@ -65,6 +65,9 @@ for part in a ab;do
 				fi
 
 				target="treble_${arch}_${part_suffix}${apps_suffix}${su_suffix}"
+				if [ "$1" == "havoc" ];then
+					target="havoc_${target}"
+				fi
 
 				baseArch="$arch"
 				if [ "$arch" = "a64" ];then
