25131d7fd7270103560d3b35d6169ee1f7c4424c
Anushek Prasal <anushekprasal@gmail.com>
2022-01-08 15:55:05 +0530

    treble: Add makefiles
    Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,8 @@
+#
+# Copyright (C) 2022 Havoc-OS
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/havoc_treble_a64.mk \
+    $(LOCAL_DIR)/havoc_treble_arm.mk \
+    $(LOCAL_DIR)/havoc_treble_arm64.mk
---
+++ havoc_treble_arm.mk
@@ -0,0 +1,20 @@
+#
+# Copyright (C) 2022 Havoc-OS
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+include build/make/target/product/aosp_arm_ab.mk
+include vendor/havoc/build/core/config.mk
+
+$(call inherit-product, device/phh/treble/base.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
+
+HAVOC_BUILD_TYPE := Official
+TARGET_GAPPS_ARCH := arm
+
+PRODUCT_NAME := havoc_treble_arm
+PRODUCT_DEVICE := phhgsi_arm_ab
+PRODUCT_BRAND := Havoc-OS
+PRODUCT_SYSTEM_BRAND := Havoc-OS
+PRODUCT_MODEL := Havoc-OS-Treble
